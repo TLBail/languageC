bfcc66a4a6c4f95c697c8ce685c84f27bb0f06d7
TLBail <62939298+TLBail@users.noreply.github.com>
2021-03-02 18:51:03 +0100

    ii

---
+++ TPOpenclassrom/charTest/charTest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+
+    for (size_t i = 35; i < 125; i++)
+    {
+        printf("char : %c\n", i);
+    }
+    
+    char lettre = 67;
+
+    printf("letre : %c\n", lettre);
+
+    return 0;
+}
